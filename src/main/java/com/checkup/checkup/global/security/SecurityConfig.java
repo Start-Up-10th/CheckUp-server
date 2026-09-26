b706b89651d62550dca6c5b35427d0ffcd1d184e
minwoo-3 <s26002@gsm.hs.kr>
2026-09-27 00:30:51 +0900

    feat: 인증되지 않은 요청에 401 응답

--- src/main/java/com/checkup/checkup/global/security/SecurityConfig.java
+++ src/main/java/com/checkup/checkup/global/security/SecurityConfig.java
@@ -2,9 +2,11 @@ package com.checkup.checkup.global.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 public class SecurityConfig {
@@ -12,6 +14,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .exceptionHandling(e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
